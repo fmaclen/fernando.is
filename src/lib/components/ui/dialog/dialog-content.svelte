62c7ec5b5032ae4efa276adf75eb0fde92663797
Fernando Maclen <fmaclen@gmail.com>
2025-12-06 13:18:40 -0500

    fix: use dvh for fullscreen dialog on iOS

--- src/lib/components/ui/dialog/dialog-content.svelte
+++ src/lib/components/ui/dialog/dialog-content.svelte
@@ -31,7 +31,7 @@
 		class={cn(
 			'bg-background data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 fixed z-50 duration-200',
 			fullscreen
-				? 'inset-0 w-screen h-screen'
+				? 'inset-0 w-screen h-[100dvh]'
 				: 'data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 start-[50%] top-[50%] grid w-full max-w-[calc(100%-2rem)] translate-x-[-50%] translate-y-[-50%] gap-4 rounded-lg border p-6 shadow-lg sm:max-w-lg',
 			className
 		)}
